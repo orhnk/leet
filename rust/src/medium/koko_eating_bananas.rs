49c99a22a4eefd510f4b01f6496c0ae7c5441fef
KoBruhh <atomoyuncu.yt@gmail.com>
2023-03-18 18:37:09 +0300

    added errounous koko 875

---
+++ rust/src/medium/koko_eating_bananas.rs
@@ -0,0 +1,42 @@
+/*
+
+875. Koko Eating Bananas
+Medium
+7.3K
+338
+Companies
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ */
+
+fn main() {
+    let piles = vec![3, 6, 7, 11];
+    let h = 4;
+    println!("{}", min_eating_speed(piles, h));
+}
+
+// impl Solution {
+
+/* This is a binary search question which is "easy" to solve */
+//                                            ^^^^- Arguably
+pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+    let (mut l, mut r) = (0, piles.iter().max().unwrap());
+
+    while l < *r {
+        let m = l + (r - l) / 2;
+        if piles.iter().map(|i| i/m).sum() <= h {
+            r = m;
+            continue
+        }
+        l = &m;
+    }
+    l
+}
+
+// }
