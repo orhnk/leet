db03bacd66fd0e570d1e7bf665e0edfd82d213f6
KoBruhh <atomoyuncu.yt@gmail.com>
2023-03-18 22:58:01 +0300

    added 338

--- rust/src/medium/koko_eating_bananas.rs
+++ rust/src/medium/koko_eating_bananas.rs
@@ -23,6 +23,27 @@ fn main() {
 
 // impl Solution {
 
+// After! (
+// Other rust solution from Solutions:
+pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+    let (mut l, mut r) = (1, *piles.iter().max().unwrap());
+    let mut res = r;
+    while l <= r{
+        let m = l + (r - l) / 2;
+        let hours = piles.iter().fold(0i32, |acc, &p|  acc + (p + m -1)/m);
+        if hours <= h{
+            res = res.min(m);
+            r = m - 1;
+        }else {
+            l = m + 1;
+        }
+    }
+    res
+}
+
+// )
+
+// Errorenous (Mine)
 /* This is a binary search question which is "easy" to solve */
 //                                            ^^^^- Arguably
 pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
@@ -30,11 +51,11 @@ pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
 
     while l < *r {
         let m = l + (r - l) / 2;
-        if piles.iter().map(|i| i/m).sum() <= h {
-            r = m;
+        if dbg!(piles.iter().fold(0i32, |acc, &p|  acc + (p + m -1)/m)) <= h {
+            let r = &m;
             continue
         }
-        l = &m;
+        l = m;
     }
     l
 }
