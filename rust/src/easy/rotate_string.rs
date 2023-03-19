0da9cb52b7f7fb8b7c7413221a2b5c7e120f24a4
KoBruhh <atomoyuncu.yt@gmail.com>
2023-03-19 08:39:01 +0300

    added 796 rust efficient btw. :D

---
+++ rust/src/easy/rotate_string.rs
@@ -0,0 +1,36 @@
+/*
+796. Rotate String
+Easy
+2.6K
+108
+Companies
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+*/
+
+fn main() {
+    let s = "pbxjbetnuarpyizxvgfgbrnhmiruxuhnyvvhawdnwoekxovasrmqacslhsgwzwdrljltnrqxvzhtlywyngwpkkxclnhkmreneduu".to_owned();
+    let g = "nyvvhawdnwoekxovasrmqacslhsgwzwdrljltnrqxvzhtlywyngwpkkxclnhkmreneduupbxjbetnuarpyizxvgfgbrnhmiruxuh".to_owned();
+    let e = true;
+    let f = Solution::rotate_string(s, g);
+
+    // assert_eq!(f, e);
+
+    print!("{f}");
+}
+
+// This could be a bit of cheating. But The question did not indicate that was immutable (in python you can do it in the safe way :even in the fastest answer:)
+struct Solution;
+impl Solution {
+    pub fn rotate_string(s: String, goal: String) -> bool {
+        unsafe {
+            return s.len() == goal.len() && {
+                (*(&goal as *const String as *mut String)).push_str(&goal);
+                goal.contains(&s)
+            };
+        }
+    }
+}
